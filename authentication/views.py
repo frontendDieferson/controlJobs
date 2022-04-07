bc1a339b26555051ab270af2f5fa2eeec46b0d20
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-04-07 20:47:29 -0300

    Implementando frontend e auth no backend

---
+++ authentication/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def cadastro(request):
+    if request.method == "GET":
+      return render(request, 'cadastro.html')
+    elif request.method == "POST":
+      username = request.POST.get('username')
+      senha = request.POST.get('password')
+      confirmar_senha = request.POST.get('confirm-password')
+
+      if not senha == confirmar_senha:
+        return redirect('/auth/cadastro')
+
+      if len(username.strip()) == 0 or len(senha.strip()) ==0:
+        return redirect('/auth/cadastro')
+
+      user = User.objects.filter(username=username)
+
+      if user.exists():
+        return redirect('auth/cadastro')
+
+      return HttpResponse('Recebido')
+
+
+def login(request):
+    pass
