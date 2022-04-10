a13dafc286fdb0d8ec3a2a6032a671f479318109
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-04-10 19:27:33 -0300

    Feature Add: Página de Perfil e Encontrar Jobs

--- jobs/models.py
+++ jobs/models.py
@@ -10,25 +10,20 @@ class Referencias(models.Model):
 
 class Jobs(models.Model):
     categoria_choices = (('D', 'Design'),
-                         ('EV', 'Edição de Vídeo'),
-                         ('MD', 'Marketing Digital'),
-                         ('LP', 'Landing Page'))
-
+                         ('EV', 'Edição de Vídeo'))
     status_choices = (('C', 'Em criação'),
                       ('AA', 'Aguardando aprovação'),
                       ('F', 'Finalizado'))
-
-
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
-    categoria = models.CharField(max_length=4, choices=categoria_choices, default="D")
+    categoria = models.CharField(max_length=2, choices=categoria_choices, default="D")
     prazo_entrega = models.DateTimeField()
     preco = models.FloatField()
     referencias = models.ManyToManyField(Referencias)
     profissional = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
     reservado = models.BooleanField(default=False)
-    status = models.CharField(max_length=4, default="AA")
+    status = models.CharField(max_length=2, default='C')
+    arquivo_final = models.FileField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.titulo
-    
